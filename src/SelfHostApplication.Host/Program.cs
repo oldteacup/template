ce3317a1af69c6a603faf5059c896fe1af917cd3
lenovo <lenovo@lenovo.com>
2023-07-25 18:43:53 +0800

    创建项目

---
+++ src/SelfHostApplication.Host/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfHostApplication.Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
