7786be3c0d8c048ca7f2ccb4445d198c038672c9
lenovo <lenovo@lenovo.com>
2023-07-26 08:53:29 +0800

    client 增加 program

---
+++ src/SelfHostApplication.WindowsClient/Program.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace SelfHostApplication.WindowsClient
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            SelfHostApplication.WindowsClient.App app = new SelfHostApplication.WindowsClient.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
--- src/SelfHostApplication.WindowsClient/View/Windows/MainWindow.xaml.cs
+++ src/SelfHostApplication.WindowsClient/View/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SelfHostApplication.WindowsClient
+namespace SelfHostApplication.WindowsClient.View.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
